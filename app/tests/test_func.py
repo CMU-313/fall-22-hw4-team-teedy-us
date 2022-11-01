a14af976f7946c0615660e71dea3ec1276315256
Anesha <aneshas@LAPTOP-CK7JVJCB.localdomain>
2022-11-01 06:15:48 -0400

    added test to check for error if attribute other than those listed is used to compute score

---
+++ app/tests/test_func.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+from app.handlers.routes import configure_routes
+
+# we are looking to use the reasons, activities, and absences attributes to predict
+
+def test_predict_route():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict?reasons="home"&absences=2&activies=yes'
+    response = client.get(url)
+    
+    assert response.status_code == 200
+    assert response.get_data() == b'1\n' #will change this number to what we should actually be predicting
+
+def test_predict_missing_queries():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict?reasons="home"&absences=2'
+    
+    response = client.get(url)
+    assert response.status_code == 400 #should return bad request 400
+
+def test_predict_extra_queries():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict?reasons="home"&absences=2&activies=yes&school=GP'
+
+    response = client.get(url)
+    assert response.status_code == 400 #should return bad request 400
