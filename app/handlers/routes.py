2ec0e4ccfd700ed8f699eec7aeb3fdf7be314932
Spencer Long <spencerlong0624@gmail.com>
2022-11-08 17:44:53 -0500

    Finished and updated predict route

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -19,15 +19,41 @@ def configure_routes(app):
     @app.route('/predict')
     def predict():
         #use entries from the query string here but could also use json
-        age = request.args.get('age')
-        absences = request.args.get('absences')
-        health = request.args.get('health')
-        data = [[age], [health], [absences]]
+        Mjob_health = request.args.get('Mjob_health', None, type=int)
+        Fjob_teacher = request.args.get('Fjob_teacher', None, type=int)
+        studytime = request.args.get('studytime', None, type=int)
+        higher_yes = request.args.get('higher_yes', None, type=int)
+        health = request.args.get('health', None, type=int)
+        absences = request.args.get('absences', None, type=int)
+
+        if Mjob_health == None or Fjob_teacher == None or studytime == None or higher_yes == None or health == None or absences == None:
+            return "Invalid Parameters or Missing Parameters", 400
+        elif Mjob_health != 1 and Mjob_health != 0:
+            return "Mjob_health has to be either 1 or 0", 400
+        elif Fjob_teacher != 1 and Mjob_health != 0:
+            return "Fjob_teacher has to be either 1 or 0", 400
+        elif studytime < 1 or studytime > 4:
+            return "studytime has to be between 1 and 4 inclusive", 400
+        elif higher_yes != 1 and higher_yes != 0:
+            return "higher_yes is to be either 1 or 0", 400
+        elif health < 1 or health > 5:
+            return "health has to be between 1 and 5 inclusive", 400
+        elif absences < 0 or absences > 93:
+            return "absences has to be between 0 and 93 inclusive", 400
+    
+        data = [[Mjob_health], [Fjob_teacher], [studytime], [higher_yes], [health], [absences]]
         query_df = pd.DataFrame({
-            'age': pd.Series(age),
+            'Mjob': pd.Series(Mjob_health),
+            'Fjob': pd.Series(Fjob_teacher),
+            'studytime': pd.Series(studytime),
+            'higher': pd.Series(higher_yes),
             'health': pd.Series(health),
             'absences': pd.Series(absences)
         })
         query = pd.get_dummies(query_df)
         prediction = clf.predict(query)
-        return jsonify(np.ndarray.item(prediction))
+        response = jsonify(np.ndarray.item(prediction))
+        
+        response.headers.add("Access-Control-Allow-Origin", "*")
+
+        return response, 200
